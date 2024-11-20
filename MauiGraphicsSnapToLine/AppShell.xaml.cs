9171afaf84c2e15631dac12cfe304b05042c3b10
Ryan Rubin <ryan.rubin@cognizant.com>
2024-11-20 16:06:12 +0800

    initial commit

---
+++ MauiGraphicsSnapToLine/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiGraphicsSnapToLine
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
